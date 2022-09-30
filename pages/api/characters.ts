fd1832bf03a6ea68518d7df0d2e1b04ccd6ef6d4
tyomhk <cheertrain15@naver.com>
2022-09-30 14:50:45 +0900

    Changes api name and added prisma client.

--- pages/api/characters.ts
+++ pages/api/characters.ts
@@ -1,13 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import prismaClient from '../../lib/prismaClient';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
 }
 
-export default function handler(
+export default function characterHandler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  console.log(prismaClient);
   res.status(200).json({ name: 'John Doe' })
 }
