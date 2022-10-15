9e299db7bb66ff510190dd609842710edac23ac9
tyomhk <cheertrain15@naver.com>
2022-10-15 11:39:33 +0900

    Solving image fetching problem that occurs on Korea IPs

--- components/CharacterList.tsx
+++ components/CharacterList.tsx
@@ -5,6 +5,7 @@ import placeholderImage from '../public/placeholder.webp';
 import imageLoader from "../lib/imageLoader";
 
 const THUMBNAIL_URL = 'https://chunithm.sega.jp/storage/chara/chunithm-new/thumbnail/';
+const THUMBNAIL_PATH = '../public/thumbnail/';
 const IMG_TYPE = '.png';
 /**
  * A list of charaters, grouped by each versions.
@@ -29,7 +30,7 @@ export const CharacterList: React.FC<ICharacterList> = (prop) => {
                 data-key={character.id}
               >
                 <Image
-                  src={imageLoader({src: THUMBNAIL_URL + character.id + IMG_TYPE, width: 216})}
+                  src={THUMBNAIL_URL + character.id + IMG_TYPE}
                   alt={character.name}
                   layout={'fill'}
                   objectFit={'contain'}
