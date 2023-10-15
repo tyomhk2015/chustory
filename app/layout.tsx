a475dd3176b40d1599859058566c212dd9c7045a
tyomhk <cheertrain15@naver.com>
2023-10-15 15:16:44 +0900

    * Fixed some meta tags abiding best practices.
    * Tidied the code format.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from '@vercel/analytics/react';
-import '../styles/globals.css'
+import '../styles/globals.css';
 import { FixedBackground } from './components/server/FixedBackground';
 import Script from 'next/script';
 
@@ -12,32 +12,41 @@ declare global {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   // https://developers.google.com/search/docs/crawling-indexing/special-tags
   // <meta name="google-site-verification" content="...">
   return (
-    <html lang="ko">
+    <html lang='ko'>
       <head>
-        <title>츄니즘 캐릭터 스토리 | Chunithm Character Story</title>
+        <title>Chunithm Character Story | 츄니즘 캐릭터 스토리</title>
         <meta charSet='UTF-8' />
-        <meta name='application-name' content='츄니즘 캐릭터 스토리 | Chunithm Character Story' />
+        <meta
+          name='application-name'
+          content='Chunithm Character Story | 츄니즘 캐릭터 스토리'
+        />
         <meta name='author' content='©SEGA' />
         <meta name='creator' content='©SEGA' />
         <meta name='copyright' content='©SEGA' />
         <meta name='publisher' content='©ChuStory' />
-        <meta name='description' content='츄니즘 캐릭터 스토리 | Chunithm Character Story | 츄니즘 스토리 | Chunithm Story' />
-        <meta name='robots' content='츄니즘 캐릭터 스토리 | Chunithm Character Story | 츄니즘 스토리 | Chunithm Story' />
+        <meta
+          name='description'
+          content='츄니즘 캐릭터 스토리 | Chunithm Character Story | 츄니즘 스토리 | Chunithm Story'
+        />
+        <meta name='robots' content='index, follow' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <meta name='keywords' content='츄니즘, Chunithm, チュウニズム, チューニズム, SEGA, セガ, 리듬게임, Rhythm Game, 音ゲー, BMS,' />
+        <meta
+          name='keywords'
+          content='츄니즘, Chunithm, チュウニズム, チューニズム, 세가, SEGA, セガ, 리듬게임, Rhythm Game, 音ゲー, BMS, 스토리, Story, ストーリー, 캐릭터, Character, キャラクター、キャラ―'
+        />
         <meta name='rating' content='general' />
         <meta name='theme-color' content='#040E26' />
         <link rel='icon' href='/favicon.ico' />
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_ID}`}
-          strategy="afterInteractive"
+          strategy='afterInteractive'
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id='google-analytics' strategy='afterInteractive'>
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){window.dataLayer.push(arguments);}
@@ -53,5 +62,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
