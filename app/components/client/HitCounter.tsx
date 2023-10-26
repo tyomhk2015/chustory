1a29e0024109e3c5f196a74ee05687b8f8e61876
tyomhk <cheertrain15@naver.com>
2023-10-27 00:49:47 +0900

    * Recovered hit counter.

--- app/components/client/HitCounter.tsx
+++ app/components/client/HitCounter.tsx
@@ -3,13 +3,13 @@
 const HitCounter = () => {
   return (
     <>
-      {/* <a href='https://hits.seeyoufarm.com'> */}
+      <a href='https://hits.seeyoufarm.com'>
         {/* eslint-disable @next/next/no-img-element */}
-        {/* <img
+        <img
           src='https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fwww.chustory.net'
           alt='Chustory hit'
         />
-      </a> */}
+      </a>
     </>
   );
 };
